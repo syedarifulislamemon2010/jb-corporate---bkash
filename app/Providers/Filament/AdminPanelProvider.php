50512dd6945e0d9485fac6c542e3f714386a512b
syedarifulislamemon2010 <syedarifulislamemon201093@gmail.com>
2026-08-13 12:15:52 +0600

    fix: remove default welcome widget from dashboard

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -11,8 +11,7 @@ use Filament\Pages\Dashboard;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
-use Filament\Widgets\AccountWidget;
-use Filament\Widgets\FilamentInfoWidget;
+
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
@@ -68,10 +67,7 @@ class AdminPanelProvider extends PanelProvider
                 fn () => view('filament.custom-styles')
             )
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\Filament\Widgets')
-            ->widgets([
-                AccountWidget::class,
-                FilamentInfoWidget::class,
-            ])
+            ->widgets([])
             ->plugins([
                 FilamentShieldPlugin::make()
                     ->gridColumns([
